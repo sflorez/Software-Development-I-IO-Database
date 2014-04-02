db19c4293d52301d9471a90e765d666bf50ba2f9
Michael <mfye@stetson.edu>
2014-04-02 19:16:55 -0400

    Stubbed out mergeSort.h with merge(), mergeSort() and binarySearch()

--- mergeSort.h
+++ mergeSort.h
@@ -5,4 +5,24 @@
 *	Michael Fye
 */
 
-#
+#ifndef MERGESORT_H
+#define MERGESORT_H
+#include <vector>
+
+/*
+*	Member function that is called by mergeSort() to merge values 
+*/
+void merge();
+
+/*
+*	Member function to sort the vector of keys for the current database
+*/
+void mergeSort();
+
+/*
+*	Member function to search through the sorted vector of keys by their ASCII value, and 
+*	returns the key's location in the vector
+*/
+int binarySearch();
+
+#endif
