fb73d7eb6239c59b2884cd95825b268a8952ced8
Antonio <aegarcia@stetson.edu>
2014-04-02 18:17:37 -0400

    Algorithm.cpp added

---
+++ Algorithm.cpp
@@ -0,0 +1,10 @@
+/*
+ * Algorithm.cpp
+ * Author: Antonio Garcia
+ * Created: 4/2/14
+ */
+
+#include<iostream>
+#include<fstream>
+#include "algorithm.h"
+
---
+++ Algorithm.h
@@ -0,0 +1,16 @@
+#ifdef ALGORITHM_H
+#define ALGORITHM_H
+
+class Algorithm
+{
+	private:
+		Algorithm()
+		{}
+	public:
+		static int add(int a, int b){return a+b;}
+	
+	virtual ~Algorithm();
+	
+};
+
+#endif
