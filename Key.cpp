5ffb9c74d609382dc3257c08c232086d6bc4a1bd
sflorez <sflorez@stetson.edu>
2014-04-02 17:46:11 -0400

    added the code to the key.cpp class

--- Key.cpp
+++ Key.cpp
@@ -2,14 +2,58 @@
  * Key.cpp
  *
  *  Created on: Apr 2, 2014
- *      Author: Sebass
+ *
  */
 
-#include "C:Key.h"
+#include "Key.h"
+#include <iostream>
+using namespace std;
 
-Key::Key() {
-	// TODO Auto-generated constructor stub
+const char* keyName;
+const char* position;
+const int lengthOfData;
 
+Key::Key( const char* key, const int pos, const int length  )
+{
+	setKey(key);
+	setPos(pos);
+	setLength(length);
+
+}
+
+const char* Key::getKey()
+{
+	return keyName;
+}
+
+void Key::setKey( const char* key)
+{
+	keyName = key;
+}
+
+const int Key::getPos()
+{
+	return position;
+}
+
+void Key::setPos( const int pos)
+{
+	position = pos;
+}
+
+const int Key::getLength()
+{
+	return lengthOfData;
+}
+
+void Key::setLength( const int length)
+{
+	lengthOfData = length;
+}
+
+long Key::getAsciiValue()
+{
+	return 0;
 }
 
 Key::~Key() {
