087c4a65aa55786f83a6e3cf7db9750734fb1499
Michael <mfye@stetson.edu>
2014-04-02 20:14:19 -0400

    mergeSort.cpp:    Expanded mergeSort(). Needs to be called initially with a tempStorage vector.

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,16 +1,24 @@
 #include 'Key.h'
 #include <vector>
 
-void merge()
+void merge(vector<Key> &iKeyVector, int iLow, int iHigh, vector<Key> &iTempStorage)
 {
 
 }
-void mergeSort(vector<Key> &keyVector)
+void mergeSort(vector<Key> &iKeyVector, int iLow, int iHigh, vector<Key> &iTempStorage)
 {
 	int sortLow, sortMid, sortHigh;
+	vector<Key> tempStorage;
+
 	sortLow = 0;
-	sortHigh = keyVector.size();
-	sortMid = sortHigh / 2;
+	sortHigh = iKeyVector.size;
 
+	if (sortLow < sortHigh)
+	{
+		sortMid = (sortHigh - sortLow) / 2;
 
+		mergeSort(iKeyVector, sortLow, sortMid, tempStorage);
+		mergeSort(iKeyVector, ++sortMid, sortHigh);
+		merge(iKeyVector, sortLow, sortMid, sortHigh, tempStorage);
+	}
 }
