5a227ab914474e4b24e1250735fb8606b2faa6f1
cdecker <cdecker@londo.ad.stetson.edu>
2014-04-07 15:40:40 -0400

    Updated the test cases, with information and more stub

--- tests/sample_unittest.cc
+++ tests/sample_unittest.cc
@@ -7,41 +7,88 @@ using namespace std;
 
 
 /**
- * Test Fixtures
- * 
+ * Test Fixtures 
  */ 
 class SimpleDBTest : public testing::Test 
 {
     protected:
         
         /**
-         * Helper method
-         */ 
-        
+         * Has the constructor create two files, keyFile and dataFile,
+         * then tests if they are there
+         * 
+         * @params: keyFile and dataFile
+         */  
         void ConstructorTester( const string &keyFile, const string &dataFile )
         {
             const char* keyFileName = keyFile.c_str();
             const char* dataFileName = dataFile.c_str();
+            SimpleDB::SimpleDB simpleDB( keyFileName, dataFileName );
             ifstream file(keyFileName);
-            ifstream file2(dataFileName);
-            
+            ifstream file1(dataFileName);
+  
             ASSERT_TRUE(file);
-            ASSERT_TRUE(file2);
+            ASSERT_TRUE(file1);
+        }
+        
+        /**
+         * 
+         * 
+         * @params: db, user, password, shift
+         */ 
+        void CreateTest( const char* db, const char* user, const char* password, const int shift )
+        {
             
-            SimpleDB::SimpleDB simpleDB( keyFile, dataFile );
-            
-            ASSERT_TRUE(file);
-            ASSERT_TRUE(file2);
+        }
+        
+        /**
+         * 
+         * 
+         * @params: db, user, password
+         */ 
+        void ConnectTest(const char* db, const char* user, const char* password)
+        {
+           
         }
 };
 
 
 /**
  * Uses the test fixture to test the "constructorTester"
+ * Using the _F alerts it to use the fixture.
  */
-TEST_F(SimpleDBTest, isFileThere)
+TEST_F(SimpleDBTest, ConstructorTest)
 {
     const string &key = "keyFile";
     const string &data = "dataFile";
     ConstructorTester( key, data );   
+}
+
+/**
+ * Calls the "CreateTest" in the fixture
+ */
+TEST_F(SimpleDBTest, CreateTest)
+{
+    const string &db = "TestDataBase";
+    const string &userName = "TestUserName";
+    const string &passwordName = "TestPassword";    
+    const int &shift = 4;
+   
+    const char* dataBase = db.c_str();
+    const char* user = userName.c_str();
+    const char* password = passwordName.c_str();
+}
+
+/**
+ * Callsed the "ConnectTest" in the fixture
+ */
+TEST_F(SimpleDBTest, ConnectTest)
+{
+    const string &db = "Database";
+    const string &userName = "TestUserName";
+    const string &passwordName = "TestPassword";
+    
+    const char* dataBase = db.c_str(); 
+    const char* user = userName.c_str();
+    const char* password = passwordName.c_str();
 }
