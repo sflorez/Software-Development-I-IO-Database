4ba6e14d7526e650e39360270074aa6e4302dbf1
Michael <mfye@stetson.edu>
2014-04-08 15:43:58 -0400

    mergeSort.cpp: Finished merge().    merge() requires the key vector, and a temporary storage vector as inputs

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,14 +1,49 @@
 #include 'Key.h'
 #include <vector>
 
-void merge(vector<Key> &iKeyVector, int iLow, int iHigh, vector<Key> &iTempStorage)
+/*
+ * Called in mergeSort()
+ * Stores values from the vector containing the keys into a temporary storage vector as the 
+ *		keys are ordered.
+ * Once completely ordered, the keys are then merged back into the original vector
+*/
+void merge(vector<Key> &iKeyVector, int iLow, int iMid, int iHigh, vector<Key> &iTempStorage)
 {
+	int left = iLow;
+	int right = iMid + 1;
 
+	int temp = iLow;
+
+	while ((left <= iMid) && (right <= iHigh))
+	{
+		if (iKeyVector[left] < iKeyVector[right])
+		{
+			iTempStorage[temp++] = iKeyVector[left++];
+		}
+		else
+		{
+			iTempStorage[temp++] = iKeyVector[right++];
+		}
+	}
+
+	while (left <= iMid)
+	{
+		iTempStorage[temp++] = iKeyVector[left++];
+	}
+
+	while (right <= iHigh)
+	{
+		iTempStorage[temp++] = iKeyVector[right++];
+	}
+
+	for (temp = temp - 1; temp >= iLow; --temp)
+	{
+		iKeyVector[temp] = iTempStorage[temp];
+	}
 }
 void mergeSort(vector<Key> &iKeyVector, int iLow, int iHigh, vector<Key> &iTempStorage)
 {
 	int sortLow, sortMid, sortHigh;
-	vector<Key> tempStorage;
 
 	sortLow = 0;
 	sortHigh = iKeyVector.size;
