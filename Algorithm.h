eb1585f4f3da158b5565c98c847947c4441037b0
Antonio <aegarcia@stetson.edu>
2014-04-09 17:36:16 -0400

    Algorithm.h updated

--- Algorithm.h
+++ Algorithm.h
@@ -1,3 +1,10 @@
+/*
+ * Author Antonio Garcia
+ * date re created April 3rd 2014
+ *
+ * Algorithm for encryption and decryption
+ *
+ */
 #ifdef ALGORITHM_H
 #define ALGORITHM_H
 
@@ -8,11 +15,13 @@ class Algorithm
 
 	public:
 		 static int	add(int a, int b);
-		
+
 	private:
+
+		 char encrypt(char* data, int shift);
+		 char decrypt(char* data, int shift);
 		 bool encrypt(char* data, int shift);
 		 bool decrypt(char* data, int shift);
-		 	 
-} 
+};
 
-#endif 
+#endif
