a3de76372d9cbcccf28c493fdd76604625b6a6e9
sflorez <sflorez@stetson.edu>
2014-04-11 22:04:52 -0400

    Me and Antonio worked to fix the merge that didnt work, still has errors but pushing up what we were able to change

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,25 +1,26 @@
-#include 'Key.h'
+#include "Key.h"
 #include <vector>
+using namespace std;
 
 /*
- * Compares two keys character by character. If it detects any character difference, 
+ * Compares two keys character by character. If it detects any character difference,
  * it computes the difference and determines which string should occur first in alphabetical
- * order. 
- * If the strings are equivalent up until a point where one string is longer than the other, 
+ * order.
+ * If the strings are equivalent up until a point where one string is longer than the other,
  * the longer string will return a larger value.
  * A return of -1 means the object should occur first.
  * A return of 1 means the object should occur last.
  * A return of 0 means the two objects are identical.
 */
-int compareASCII(const char* &iKey1, const char* &iKey2)
+int compareASCII(const char* iKey1, const char* iKey2)
 {
-	for (int i = 0; i < strlen(iKey1) && i < strlen(iKey2); i++)
+	for (unsigned int i = 0; i < strlen(iKey1) && i < strlen(iKey2); i++)
 	{
-		if (static_cast<int>(iKey1[i]) < static_cast<int>(iKey2[i]))
+		if ((int)(iKey1[i]) < (int)(iKey2[i]))
 		{
 			return -1;
 		}
-		else if (static_cast<int>(iKey1[i]) > static_cast<int>(iKey2[i]))
+		else if ((int)(iKey1[i]) > (int)(iKey2[i]))
 		{
 			return 1;
 		}
@@ -39,69 +40,84 @@ int compareASCII(const char* &iKey1, const char* &iKey2)
 
 /*
  * Called in mergeSort()
- * Stores values from the vector containing the keys into a temporary storage vector as the 
+ * Stores values from the vector containing the keys into a temporary storage vector as the
  *		keys are ordered.
  * Once completely ordered, the keys are then merged back into the original vector
 */
-void merge(vector<Key> &iKeyVector, int iLow, int iMid, int iHigh, vector<Key> &iTempStorage)
+void merge(vector<Key*> &iKeyVector, int iLow, int iMid, int iHigh, vector<Key*> &iTempStorage)
 {
 	int left = iLow;
 	int right = iMid + 1;
 
-	int temp = iLow;
+	int tempLow = iLow;
 
 	while ((left <= iMid) && (right <= iHigh))
 	{
-		if (compareASCII(iKeyVector[left].getKey(), iKeyVector[right].getKey()) < 0)
+		if (compareASCII(iKeyVector[left]->getKey(), iKeyVector[right]->getKey()) < 0)
 		{
-			iTempStorage[temp++] = iKeyVector[left++];
+			iTempStorage.at(tempLow)= iKeyVector.at(left);
+			left++;
 		}
 		else
 		{
-			iTempStorage[temp++] = iKeyVector[right++];
+			iTempStorage.at(tempLow) = iKeyVector.at(right);
+			right++;
+		}
+		tempLow ++;
+	}
+
+	if(left > iMid)
+	{
+		for ( unsigned int k = right ; k <= iHigh ; k ++ )
+		{
+			iTempStorage.at(tempLow) = iKeyVector.at(k);
+			tempLow++;
+		}
+
+	}
+
+	else
+	{
+		for( unsigned int k = left; k <=iMid; k ++)
+		{
+			iTempStorage.at(tempLow)= iKeyVector.at(k);
+			tempLow++;
 		}
 	}
 
-	while (left <= iMid)
+	for (tempLow = tempLow - 1; tempLow >= iLow; tempLow--)// changed --
 	{
-		iTempStorage[temp++] = iKeyVector[left++];
-	}
-
-	while (right <= iHigh)
-	{
-		iTempStorage[temp++] = iKeyVector[right++];
-	}
-
-	for (temp = temp - 1; temp >= iLow; --temp)
-	{
-		iKeyVector[temp] = iTempStorage[temp];
+		iKeyVector.at(tempLow) = iTempStorage.at(tempLow);
 	}
 }
 
 /*
  * mergeSort function. Requires a vector of keys, the beginning and end of the  segment (as an int to be used to reference the vector)
- *		and a temporary vector for storage to merge the keys 
+ *		and a temporary vector for storage to merge the keys
 */
-void mergeSort(vector<Key> &iKeyVector, int iLow, int iHigh, vector<Key> &iTempStorage)
+void mergeSort(vector<Key*> &iKeyVector, int iLow, int iHigh, vector<Key*> &iTempStorage)
 {
 	int sortLow, sortMid, sortHigh;
 
-	sortLow = 0;
-	sortHigh = iKeyVector.size;
+	sortLow = iLow;
+	sortHigh = iHigh;
+
 
 	if (sortLow < sortHigh)
 	{
-		sortMid = (sortHigh - sortLow) / 2;
+		sortMid = (sortHigh + sortLow) / 2;
 
-		mergeSort(iKeyVector, sortLow, sortMid, tempStorage);
-		mergeSort(iKeyVector, ++sortMid, sortHigh);
-		merge(iKeyVector, sortLow, sortMid, sortHigh, tempStorage);
+		mergeSort(iKeyVector, sortLow, sortMid, iTempStorage);
+		mergeSort(iKeyVector, sortMid +1 , sortHigh , iTempStorage);
+		merge(iKeyVector, sortLow, sortMid, sortHigh, iTempStorage);
 	}
 
+}
+
 	/*
-	 * binarySearch takes in a vector of key objects and a const char* iTerm. 
+	 * binarySearch takes in a vector of key objects and a const char* iTerm.
 	 * Searches through the vector and compares the ASCII values of the keys in the vector with iTerm
-	 * 
+	 *
 	 * Returns the position of the key in the vector if found
 	 * Returns -1 if specified key does not exist
 	*/
@@ -130,4 +146,4 @@ void mergeSort(vector<Key> &iKeyVector, int iLow, int iHigh, vector<Key> &iTempS
 		}
 		return -1;
 	}
-}
+
--- mergeSort.h
+++ mergeSort.h
@@ -2,28 +2,27 @@
 *	mergeSort.h
 *	Headerfile for mergeSort.cpp
 *
-*	Michael Fye
 */
 
 #ifndef MERGESORT_H
 #define MERGESORT_H
 #include <vector>
-#include 'Key.h'
+#include "Key.h"
 
 /*
-*	Member function that is called by mergeSort() to merge values 
+*	Member function that is called by mergeSort() to merge values
 */
-void merge( vector<Key> &iKeyVector, int iLow, int iMid, int iHigh, vector<Key> &iTempStorage);
+void merge( std::vector<Key*> &iKeyVector, int iLow, int iMid, int iHigh, std::vector<Key*> &iTempStorage);
 
 /*
 *	Member function to sort the vector of keys for the current database
 */
-void mergeSort(vector<Key> &iKeyVector, int iLow, int iHigh, vector<Key> &iTempStorage);
+void mergeSort(std::vector<Key*> &iKeyVector, int iLow, int iHigh, std::vector<Key*> &iTempStorage);
 
 /*
-*	Member function to search through the sorted vector of keys by their ASCII value, and 
+*	Member function to search through the sorted vector of keys by their ASCII value, and
 *	returns the key's location in the vector
 */
-int binarySearch(vector<Key> &iKeyVector, const char* iTerm);
+int binarySearch(std::vector<Key*> &iKeyVector, const char* iTerm);
 
 #endif
