2e2a5aa21fd9487541382bca0384b6cecf27dcb3
Michael Fye <mfye@stetson.edu>
2014-04-13 18:44:00 -0400

    Altered and tested mergeSort.cpp. Now sorts and searches by ASCII values

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -2,27 +2,43 @@
 #include <cstring>
 #include <vector>
 using namespace std;
-
-
+  
+  
 /*
  * Called in mergeSort()
  * Stores values from the vector containing the keys into a temporary storage vector as the
- *		keys are ordered.
+ *      keys are ordered.
  * Once completely ordered, the keys are then merged back into the original vector
  * Sorts based on the length of the keyName.
 */
-void merge(vector<Key*> &keyVector,vector<Key*> &tempStorage, int low, int pivot, int high );
-void mergeSort(vector<Key*> &keyVector, vector<Key*> &tempStorage, int low, int high)
+  
+int compareASCII(const char *iKey1, const char *iKey2)
 {
-    int pivot;
-    if(low<high)
+    for( ; *iKey1 != '\0' && *iKey2 != '\0'; iKey1++, iKey2++)
     {
-        pivot=(low+high)/2;
-        mergeSort( keyVector, tempStorage,low,pivot);
-        mergeSort(keyVector,tempStorage,pivot+1,high);
-        merge(keyVector,tempStorage,low,pivot,high);
+        if(*iKey1 < *iKey2)
+        {
+            return -1;
+        }
+        if(*iKey1 > *iKey2)
+        {
+            return 1;
+        }
+    }
+    if(*iKey2 != '\0')
+    {
+        return -1;
+    }
+    else if(*iKey1 != '\0')
+    {
+       return 1;
+    }
+    else
+    {
+      return 0;
     }
 }
+  
 void merge(vector<Key*> &keyVector, vector<Key*> &tempStorage, int low, int pivot, int high)
 {
     int left,tempLow,right,k;
@@ -32,7 +48,8 @@ void merge(vector<Key*> &keyVector, vector<Key*> &tempStorage, int low, int pivo
 
     while((left<=pivot)&&(right<=high))
     {
-        if(strlen(keyVector[left]->getKey())<= strlen(keyVector[right]->getKey()))
+        //(int)keyVector[left]->getKey()) <= ...
+        if(compareASCII(keyVector[left]->getKey(), keyVector[right]->getKey()) < 0)
         {
             tempStorage[tempLow]=keyVector[left];
             left++;
@@ -43,9 +60,9 @@ void merge(vector<Key*> &keyVector, vector<Key*> &tempStorage, int low, int pivo
             right++;
         }
         tempLow++;
-    }
-    if(left>pivot)
-    {
+  }
+  if(left>pivot)
+  {
         for(k=right; k<=high; k++)
         {
             tempStorage[tempLow]= keyVector[k];
@@ -60,41 +77,52 @@ void merge(vector<Key*> &keyVector, vector<Key*> &tempStorage, int low, int pivo
             tempLow++;
         }
     }
-    for(k=low; k<=high; k++)
-    	{
-    		keyVector[k]=tempStorage[k];
-    	}
-}
-
-	/*
-	 * Searches based on the length of the keyName.
-	 * Tweeked by Michael. Should take in a vector of Key objects and an int, stating the size of the key requested
-	 * Returns the int of the Key's location in the vector. 
-	 * Returns -1 if the key is not found. 
-	*/
-int binarySearch(vector<Key*> &iKeyVector, int iKeyLength)
-	{
-		int keyVectLength = iKeyVector.size();
-		int low, high;
-		low = 0;
-		high = iKeyVector.size() - 1;
-
-		while (low <= high)
-		{
-			int mid = (low + high) / 2;
-			if (strlen(iKeyVector[mid]->getKey()) == iKeyLength)
-			{
-				return mid;
-			}
-			else if (strlen(iKeyVector[mid]->getKey()) < iKeyLength)
-			{
-				high = mid - 1;
-			}
-			else
-			{
-				low = mid + 1;
-			}
-		}
-		return -1;
-}
-
+      for(k=low; k<=high; k++)
+          {
+              keyVector[k]=tempStorage[k];
+          }
+  }
+  
+  void mergeSort(vector<Key*> &keyVector, vector<Key*> &tempStorage, int low, int high)
+  {
+      int pivot;
+      if(low<high)
+      {
+          pivot=(low+high)/2;
+          mergeSort( keyVector, tempStorage,low,pivot);
+          mergeSort(keyVector,tempStorage,pivot+1,high);
+          merge(keyVector,tempStorage,low,pivot,high);
+      }
+  }
+  
+     /*
+      * Searches based on the length of the keyName.
+      * Tweeked by Michael. Should take in a vector of Key objects and an int, stating the size of the key requested
+      * Returns the int of the Key's location in the vector. 
+     */
+ int binarySearch(vector<Key*> &iKeyVector, const char* iKey)
+     {
+         int keyVectLength = iKeyVector.size();
+         int keyLength = strlen(iKey);
+         int low, high;
+         low = 0;
+         high = iKeyVector.size() - 1;
+ 
+         while (low <= high)
+         {
+             int mid = (low + high) / 2;
+             if (compareASCII(iKey, iKeyVector[mid]->getKey()) == 0)
+             {
+                 return mid;
+             }
+             else if (compareASCII(iKey, iKeyVector[mid]->getKey()) < 0)
+             {
+                 high = mid - 1;
+             }
+             else
+             {
+                 low = mid + 1;
+             }
+         }
+         return -1;
+}​
