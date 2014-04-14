13a38a7eab925b85239b90d5cb7ac577bfd4409b
Antonio <aegarcia@stetson.edu>
2014-04-13 23:05:50 -0400

    woking on merge test cases, not finshed

--- tests/sample_unittest.cc
+++ tests/sample_unittest.cc
@@ -62,7 +62,7 @@ class allTest : public testing::Test
             ASSERT_EQ(correct, buffer);
         }
         
-        /**
+        /*
          * 
          * @params: db, user, password
          */
@@ -73,14 +73,14 @@ class allTest : public testing::Test
             i_DatabaseFile = "dataFile";
                            
             SimpleDB simpleDB( i_KeyFileName, i_DatabaseFile );
-            //simpleDB.connect( db, user, password);
+            simpleDB.connect( db, user, password);
             ASSERT_FALSE(simpleDB.getConnect());
             
-            //simpleDB.connect( db, user, password);
+            simpleDB.connect( db, user, password);
             
             ASSERT_TRUE(simpleDB.getConnect());
         }
-        /**
+        /*
          * 
          * 
          * @params: key, value 
@@ -94,8 +94,8 @@ class allTest : public testing::Test
             i_password = "TestPassword";
             const int shift = 4;
             SimpleDB simpleDB( i_KeyFileName, i_DatabaseFile );
-          //  simpleDB.connect( i_db, i_user, i_password);
-            //simpleDB.create( i_db, i_user, i_password, shift );
+            simpleDB.connect( i_db, i_user, i_password);
+            simpleDB.create( i_db, i_user, i_password, shift );
             ASSERT_TRUE(simpleDB.insert( key, value ));
         }
         
@@ -109,11 +109,18 @@ class allTest : public testing::Test
         {
             ASSERT_TRUE( Algorithm::decrypt( data, shift) );
         }
-        
+        void EncryptionAsciiBounds(char* data, int shift)
+        {
+            ASSERT_FALSE(Algorithm::encrypt(data, shift));
+        }
+         void DecryptionAsciiBounds(char* data, int shift)
+        {
+            ASSERT_FALSE(Algorithm::encrypt(data, shift));
+        }
 };
 
 
-/**
+/*
  * Uses the test fixture to test the "constructorTester"
  * Using the _F alerts it to use the fixture.
  */
@@ -124,7 +131,7 @@ TEST_F(allTest, ConstructorTest)
     ConstructorTester( key, data );
 }
 
-/**
+/*
  * Calls the "connectTest"
  */
 TEST_F(allTest, connectTest)
@@ -134,12 +141,11 @@ TEST_F(allTest, connectTest)
     const char* password = "TestPassword";
     connectTest( db, user, password );
 }
-      
 
-/**
+
+/*
  * Calls the "CreateTest" in the fixture
  */
-
 
 TEST_F(allTest, CreateTest)
 {
@@ -150,7 +156,7 @@ TEST_F(allTest, CreateTest)
     CreateTest( dataBase, user, password, shift);
 }
 
-/**
+/*
  * Called the "insert" in the fixture
  */
 
@@ -161,15 +167,20 @@ TEST_F(allTest, InsertTest)
     InsertTest( key, value) ;
 }
 
-
+/*
+ * Tests Encryption to passes with correct inputs
+ */
 TEST_F(allTest, EncryptTest)
 {
     char* key = new char[4];
-    strcpy(key, "key1"); 
+    strcpy(key, "key1");
     int shift = 4;
     EncryptTest( key, shift );
 }
 
+/*
+ * Tests if encryption passes with correct inputs
+ */
 TEST_F(allTest, DecryptTest)
 {
     char* key = new char[4];
@@ -178,3 +189,100 @@ TEST_F(allTest, DecryptTest)
     DecryptTest( key, shift );
 }
 
+/*
+ * Tests the bounds of the ascii values are within
+ * 32-126
+ */
+TEST_F(allTest, EncryptionAsciiBounds)
+{
+    char* key = new char[7];
+    strcpy(key,"áBadKey");
+    int shift = 3;
+    EncryptionAsciiBounds(key, shift);
+}
+
+/*
+ * Tests the bounds of the ascii values are within
+ * 32-126
+ */
+TEST_F(allTest, DecryptionAsciiBounds)
+{
+    char* key = new char[7];
+    strcpy(key, "áBadKey");
+    int shift = 3;
+    DecryptionAsciiBounds(key, shift);
+}
+
+/*
+ * Tests Merge Sorting
+ */
+TEST_F(allTest, mergeSortTest)
+{
+    :vector<Key*> keys;
+    string name1 = "zergRush";
+    string name2 = "Antonio";
+    string name3 = "Sebastian";
+    string name4 = "Christian";
+    
+    for(unsigned int i = 1; i < 5; i++)
+    {
+        keys.push_back(new Key(name+(string)i,1,1))
+    }
+    for(unsigned int i = 0;i<4;i++)
+    {
+        cout << keys[i]->getKey() << endl;
+    }
+    
+}
+#include<string>
+#include<cstring>
+#include <iostream>
+#include "mergeSort.h"
+#include "Key.h"
+#include <vector>
+using namespace std;
+
+vector <Key*> keys;
+int main()
+{
+    string name = "name1thelongest";
+    string name2 = "sebastian";
+    string name3 = "Anton";
+    string name4 = "bleh";
+    string name5 = "bob";
+    
+    keys.push_back(new Key(name ,1,1)); 
+    keys.push_back(new Key(name2 ,1,1));
+    keys.push_back(new Key(name3 ,1,1));
+    keys.push_back(new Key(name4 ,1,1));
+    keys.push_back(new Key(name5 ,1,1));
+    
+    cout<< "B4 merge"<< endl;
+   
+    for(unsigned int i = 0; i < 5; i++ )
+    {
+        cout << keys[i]->getKey()<<endl;
+    }        
+    
+    vector<Key*> temp = keys;
+    int num = keys.size();
+    mergeSort(keys,temp,0,num-1);
+    cout<< "After merge"<<endl;
+    
+    for(unsigned int i = 0; i < 5; i++ )
+    {
+        cout << keys[i]->getKey()<<endl;
+    }        
+    string inputName;
+    cout<< "binary search enter a name to find: "<< endl;
+    cin>> inputName;
+    cout << inputName << endl;
+   
+    if(binarySearch(keys, inputName.c_str()) < 0)
+    {
+        cout << "Noooope" << endl;
+    }
+    else
+    {
+         cout << keys[binarySearch(keys, inputName.c_str())]->getKey() << endl; 
+    }
